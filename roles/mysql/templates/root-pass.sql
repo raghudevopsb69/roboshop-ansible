4b6f1354c3c85449192e14cfb0e15cfa2b4a8e00
Raghu K <raghuk.vit@gmail.com>
2022-12-07 07:42:55 +0530

    Update roboshop-ansible/

--- roles/mysql/templates/root-pass.sql
+++ roles/mysql/templates/root-pass.sql
@@ -1,2 +1,3 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY '{{ROOT_PASSWORD}}';
 ALTER USER 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';

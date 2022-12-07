a492be7f9fc32abf176757ca63f512b726310c62
Raghu K <raghuk.vit@gmail.com>
2022-12-07 08:06:08 +0530

    Update roboshop-ansible/

--- roles/mysql/templates/root-pass.sql
+++ roles/mysql/templates/root-pass.sql
@@ -1,7 +1,1 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY '{{ROOT_PASSWORD}}';
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
-
-create database if not exists cities;
-GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';
-GRANT ALL ON cities.* TO 'shipping'@'localhost' IDENTIFIED BY 'secret';
